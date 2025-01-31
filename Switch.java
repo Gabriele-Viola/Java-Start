7eeacc081f68f663d61aa1f931b622504443096a
Gabriele-Viola <gabrieleviola50@gmail.com>
2025-01-31 12:03:10 +0100

    switch

---
+++ Switch.java
@@ -0,0 +1,24 @@
+public class Switch {
+    // You can use a 'switch' statement because, in this case, using 'if' may make
+    // your code too long
+    public static void main(String[] args) {
+        String nome = "Anna";
+        switch (nome) {
+            case "Giovanni":
+                System.out.println("Ciao Giovanni");
+                break;
+            case "Anna":
+                System.out.println("Ciao Anna");
+                break;
+            case "Paolo":
+                System.out.println("Ciao Paolo");
+                break;
+            case "Elena":
+                System.out.println("Ciao Elena");
+                break;
+            case "Gianni":
+                System.out.println("Ciao Gianni");
+                break;
+        }
+    }
+}
